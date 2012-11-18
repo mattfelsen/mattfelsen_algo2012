aeb2399641f32ebeea9d476f1228c458cc14c8df
Matt Felsen <mattfelsen@gmail.com>
2012-11-18 17:53:16 -0500

    adding soda creatures

---
+++ week8-sodaCreature/src/creature.h
@@ -0,0 +1,39 @@
+//
+//  creature.h
+//  muscles
+//
+//  Created by Matt on 11/18/12.
+//
+//
+
+#ifndef __muscles__creature__
+#define __muscles__creature__
+
+#include <iostream>
+#include "ofMain.h"
+#include "particle.h"
+#include "spring.h"
+
+class creature {
+
+public:
+	
+	void setup(ofColor color, float size);
+	void update();
+	void draw();
+	
+	void createSpring(particle * p1, particle * p2);
+
+	ofPoint pos;
+	float size;
+
+	ofColor color;
+	
+	float sinOffset;
+	
+	vector <particle * > particles;
+	vector <spring> springs;
+	
+};
+
+#endif /* defined(__muscles__creature__) */
---
+++ week8-sodaCreature/src/testApp.cpp
@@ -0,0 +1,92 @@
+#include "testApp.h"
+
+
+//--------------------------------------------------------------
+void testApp::setup(){	
+	
+	ofBackground(255);
+	
+	ofSetVerticalSync(true);
+	ofSetFrameRate(60);
+	ofEnableAlphaBlending();
+	ofEnableSmoothing();
+	
+	int basisColor = ofRandom(255);
+	int colors = 3;
+	
+	// creature a bunch of creatures
+	for (int i = 0; i < 50; i++) {
+		creature c;
+		float size;
+		
+		// make a few bigger guys and then a bunch of smaller cuties
+		if (i < 3)
+			size = ofRandom(75, 100);
+		else
+			size = ofRandom(25, 30);
+		
+		// give them random fun, bright, semi-transparent colors
+		// with the infamous Matt Felsen triadic color palette algorithm
+		float hue = fmod(basisColor + i%colors * (255/colors) + ofRandom(20), 255);
+		ofColor col = ofColor::fromHsb(hue, 200, 200, 200);
+		c.setup(col, size);
+
+		creatures.push_back(c);
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+	// on every frame 
+	// we reset the forces
+	// add in any forces on the particle
+	// perfom damping and
+	// then update
+	
+	for (int i = 0; i < creatures.size(); i++)
+		creatures[i].update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	
+	for (int i = 0; i < creatures.size(); i++)
+		creatures[i].draw();
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){ 
+	
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased  (int key){ 
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+//	particles[0]->pos.set(mouseX, mouseY);
+	/*particles.erase(particles.begin());
+	particle myParticle;
+	myParticle.setInitialCondition(x,y,0,0);
+	particles.push_back(myParticle);*/
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+//	particles[0]->bFixed = true;
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(){
+	
+//	particles[0]->bFixed = false;
+}
+
